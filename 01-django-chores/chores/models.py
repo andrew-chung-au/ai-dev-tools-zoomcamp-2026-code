de40df4d6011ea006a25868722c206bc1bc1447a
Andrew Chung <andrew.chung.au@gmail.com>
2026-09-02 17:54:01 +0000

    Implement chore rotation logic and unit tests

--- 01-django-chores/chores/models.py
+++ 01-django-chores/chores/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Person(models.Model):
@@ -31,3 +32,23 @@ class Chore(models.Model):
 
     def __str__(self):
         return self.name
+
+    def mark_done(self):
+        """Record completion and rotate assignment to the next person.
+
+        Simple round-robin over Person.order (with wraparound), no
+        skip/unavailability support. If no people exist, only
+        last_done_at is updated.
+        """
+        self.last_done_at = timezone.now()
+
+        people = list(Person.objects.all())
+        if people:
+            try:
+                current_index = people.index(self.assigned_to)
+                next_index = (current_index + 1) % len(people)
+            except ValueError:
+                next_index = 0
+            self.assigned_to = people[next_index]
+
+        self.save()
--- 01-django-chores/chores/tests.py
+++ 01-django-chores/chores/tests.py
@@ -1,3 +1,84 @@
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Chore, Person
+
+
+class MarkDoneTests(TestCase):
+    def setUp(self):
+        self.alice = Person.objects.create(name="Alice", order=0)
+        self.bob = Person.objects.create(name="Bob", order=1)
+        self.carol = Person.objects.create(name="Carol", order=2)
+
+    def test_mark_done_records_last_done_at(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.alice)
+        self.assertIsNone(chore.last_done_at)
+
+        before = timezone.now()
+        chore.mark_done()
+        after = timezone.now()
+
+        self.assertIsNotNone(chore.last_done_at)
+        self.assertGreaterEqual(chore.last_done_at, before)
+        self.assertLessEqual(chore.last_done_at, after)
+
+    def test_mark_done_advances_to_next_person(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.alice)
+
+        chore.mark_done()
+
+        self.assertEqual(chore.assigned_to, self.bob)
+
+    def test_mark_done_wraps_around_past_last_person(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.carol)
+
+        chore.mark_done()
+
+        self.assertEqual(chore.assigned_to, self.alice)
+
+    def test_mark_done_full_rotation_cycle(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.alice)
+
+        chore.mark_done()
+        self.assertEqual(chore.assigned_to, self.bob)
+
+        chore.mark_done()
+        self.assertEqual(chore.assigned_to, self.carol)
+
+        chore.mark_done()
+        self.assertEqual(chore.assigned_to, self.alice)
+
+    def test_mark_done_persists_changes(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.alice)
+
+        chore.mark_done()
+        chore.refresh_from_db()
+
+        self.assertEqual(chore.assigned_to, self.bob)
+        self.assertIsNotNone(chore.last_done_at)
+
+    def test_mark_done_with_no_people_leaves_assignment_unset(self):
+        Person.objects.all().delete()
+        chore = Chore.objects.create(name="Dishes", assigned_to=None)
+
+        chore.mark_done()
+
+        self.assertIsNone(chore.assigned_to)
+        self.assertIsNotNone(chore.last_done_at)
+
+    def test_mark_done_single_person_stays_assigned(self):
+        Person.objects.exclude(pk=self.alice.pk).delete()
+        chore = Chore.objects.create(name="Dishes", assigned_to=self.alice)
+
+        chore.mark_done()
+        self.assertEqual(chore.assigned_to, self.alice)
+
+        chore.mark_done()
+        self.assertEqual(chore.assigned_to, self.alice)
+
+    def test_mark_done_with_unassigned_chore_assigns_first_person(self):
+        chore = Chore.objects.create(name="Dishes", assigned_to=None)
+
+        chore.mark_done()
+
+        self.assertEqual(chore.assigned_to, self.alice)
